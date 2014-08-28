90b9e08ac14a03f8750147ad24c43de7bbd54a37
Rafael Khan <raf.khan.f@gmail.com>
2014-08-28 05:44:38 -0400

    featuring buttons!

--- simultaneous-playback/simplay.js
+++ simultaneous-playback/simplay.js
@@ -1,6 +1,6 @@
 var audio_dir = "/audio/";
-var song_a = "My_Nu_Leng-The_Grid_Original_Mix.mp3";
-var song_b = "Get Free (Andy C Remix).mp3";
+var song_a = audio_dir + "My_Nu_Leng-The_Grid_Original_Mix.mp3";
+var song_b = audio_dir + "Get Free (Andy C Remix).mp3";
 
 
 window.onload = function() {
@@ -16,18 +16,40 @@ window.onload = function() {
     return;
   }
 
-  var request = new XMLHttpRequest();
-  request.open("GET", audio_dir + song_b, true);
-  request.responseType = "arraybuffer";
+  function loadAudio(url, cb) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
 
-  request.onload = function() {
-    context.decodeAudioData(request.response,
-        function(buf) {
-          test(buf);
-        });
-  };
+    request.onload = function() {
+      context.decodeAudioData(request.response,
+          function(buf) {
+            var src = context.createBufferSource();
+            src.buffer = buf;
+            cb(src);
+          });
+    };
 
-  request.send();
+    request.send();
+  }
+
+  loadAudio(song_a, function(s1) {
+    loadAudio(song_b, function(s2) {
+      s1.connect(context.destination);
+      s2.connect(context.destination);
+
+      $("#a").click(function() {
+        s1.start(0);
+      });
+
+      $("#b").click(function() {
+        s2.start(0);
+      });
+
+      console.log("ready for playback!");
+    });
+  });
+
 };
 
 
